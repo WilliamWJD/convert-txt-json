faa4c04b448d47d6d7fd9f9544502b30d28403d4
williamwjd <william.jose.wjd@gmail.com>
2021-09-07 12:57:24 -0300

    separando algumas responsabilidades do server.js

--- src/server.js
+++ src/server.js
@@ -1,29 +1,10 @@
 const express = require('express');
-const multer = require('multer');
-const uploadConfig = require('./config/upload');
-const fs = require('fs');
-
-const { convertTxtToJson } = require('./services/ConvertTxtToJson');
+const routes = require('./routes/route');
 
 const app = express();
 
+app.use(routes)
 app.use(express.json());
-
-const upload = multer(uploadConfig.upload());
-
-app.post('/upload-txt', upload.single('file'), async (req, res)  => {
-    const { file } = req;
-
-    const blacklist = await convertTxtToJson(file);
-
-    fs.writeFile('blacklist.json', JSON.stringify(blacklist), (err)=>{
-        if(err){
-            return res.status(500).json({ error: "Error in generated blacklist.json"});
-        }
-
-        return res.status(201).json({ message:"Json generated successfully" })
-    })
-})
 
 app.listen(3333, () => {
     console.log('Servidor online')
